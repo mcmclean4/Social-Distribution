4b362623f855b614d381c4fe3969f1411ab7ff8d
sakib <sakibrahman0@gmail.com>
2025-02-17 18:59:33 -0700

    Add model skeletons

--- app/social/models.py
+++ app/social/models.py
@@ -1,3 +1,70 @@
+from enum import Enum
+
 from django.db import models
 
 # Create your models here.
+
+# =============================================================================
+# Author: Represents a user (local or remote) who can post, follow, etc.
+# =============================================================================
+class Author(models.Model):
+    type = models.CharField(max_length=50)
+    # The full API URL for the author. This is unique.
+    id = models.URLField(unique=True)
+    host = models.URLField(unique=True)
+    # The node that “owns” this author. For remote authors, this points to their home node.
+    displayName = models.CharField(max_length=255)
+    github = models.URLField(blank=True, null=True)
+    profileImage = models.URLField(blank=True, null=True)
+    page = models.URLField(blank=True, null=True)  # HTML profile page
+    # Flag to indicate if the author is a node administrator.
+    isAdmin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.displayName
+
+class Follow(models.Model):
+    type = models.CharField(max_length=50)
+    summary = models.CharField()
+    actor = models.ForeignKey(Author, on_delete=models.CASCADE)
+    objects = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    type = models.CharField(max_length=50)
+
+class Comments(models.Model):
+    type = models.CharField(max_length=50)
+
+class Like(models.Model):
+    type = models.CharField(max_length=50)
+
+class Likes(models.Model):
+    type = models.CharField(max_length=50)
+
+class Visibility(Enum):
+    PUBLIC = "PUBLIC"
+    PRIVATE = "PRIVATE"
+    UNLISTED = "UNLISTED"
+    DELETED = "DELETED"
+
+class Post(models.Model):
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
+    id = models.URLField(unique=True)
+    page = models.URLField(blank=True, null=True)
+    description = models.CharField()
+    contentType = models.CharField()
+    content = models.CharField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
+    likes = models.ForeignKey(Likes, on_delete=models.CASCADE)
+    published = models.DateTimeField()
+    visibility = models.Choices(Visibility)
+
+class Posts(models.Model):
+    type = models.CharField(max_length=50)
+    pageNumber = models.IntegerField()
+    size = models.IntegerField()
+    count = models.IntegerField()
+    src = models.ForeignKey(Post, on_delete=models.CASCADE)
+
