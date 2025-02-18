8854020a2c1844da6c574e8bc6159f1e84b29d51
sakib <sakibrahman0@gmail.com>
2025-02-17 19:26:49 -0700

    Add skeleton url path

--- app/social/urls.py
+++ app/social/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'social'
 urlpatterns = [
-    path("", views.stream, name="stream")
+    #
+    path("", views.stream, name="stream"),
+    # API paths
+    path("api/<str:pk>/", name="author")    # Sample api path for your feature
+
 ]
